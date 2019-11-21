2b2146641000ff3c66dd6251f2e813ca2f51f78b
Tom Banish <tombanish@gmail.com>
2019-11-21 02:39:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,7 +64,7 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   user_input = input_to_index(user_input)
-  board[user_input] = char
+  char = board[user_input]
   if valid_move?(board, user_input)
     move(board, user_input, char)
     display_board(board)
